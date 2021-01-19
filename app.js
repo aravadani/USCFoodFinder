f128fc908eab425242df3ef9b3efd191ec6d86ae
aravadani <aravadani@users.noreply.github.com>
2021-01-19 03:36:39 -0800

    default get update

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
+// Imports
 const express = require('express');
 const app = express();
 
+// Constants
 const PORT = process.env.PORT || 4000
 
+// Default Get 
+app.get('/', (req, res) => {
+  res.send(`<title>USC Food Finder</title><h1>USC Food Finder running w/ NodeJS & ExpressJS on port ${PORT}</h1>`);
+})
+
+// Server Listen
 const server = app.listen(PORT, () => {
   console.log(`Server running at port ${server.address().port}`);
 });
